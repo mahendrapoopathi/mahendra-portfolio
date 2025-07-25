3879dbcfe19165c297ebd8d463eadf3625ae5e10
Mahendra Poopathi S <mahendrapoopathi.s.2023.csbs@ritchennai.edu.in>
2025-07-25 14:46:55 +0530

    Fix: Corrected clientsData import and cleaned AboutMeContext

--- src/context/AboutMeContext.jsx
+++ src/context/AboutMeContext.jsx
@@ -1,7 +1,8 @@
 import { useState, createContext } from 'react';
 import { aboutMeData } from '../data/aboutMeData';
 import { clientsHeading as clientsPageHeading } from '../data/clientsData';
-import { clientsData as clientsDataJson } from '../data/clientsData';
+import clientsDataJson from '../data/clientsData';
+
 
 const AboutMeContext = createContext();
 
